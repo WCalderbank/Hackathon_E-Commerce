6aa326a7dd7fb35b8c2632d287f9bfdca6d498fa
Erdogan90 <94233632+Erdogan90@users.noreply.github.com>
2022-06-10 11:02:10 +0100

    pulling of data

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,7 +1,42 @@
 import "./App.css";
+import { useState, useEffect } from "react"
 
 function App() {
-  return <div className="App">Hi team</div>;
+
+  const[data, setData] = useState('')
+
+  useEffect(()=>{
+
+    const fetchData = async () =>{
+      const res = await fetch ("https://fakestoreapi.com/products")
+      const data = await res.json()
+      setData(data)
+    }
+    fetchData()
+  }, [])
+
+ 
+
+
+  console.log(data)
+
+
+  return <div className="App">Hi team
+      <h1>Welcome to NORTH and SOUTH</h1>
+      <div>
+        {data.map((a, id)=>(
+          <div key={id}>
+          <h4>
+            {a.title}
+            </h4>
+          <img src={a.image} alt={a.category}/>
+          <p>{a.description}</p>
+            <h6>{a.category}</h6>
+          </div>
+        )
+        )}
+      </div>
+      </div>;
 }
 
 export default App;
--- e-commerce/src/components/quickTest.test.js
+++ e-commerce/src/components/quickTest.test.js
@@ -1,5 +1,5 @@
-const sum = require(“./simpleSum”);
+import sum from "./quickTest.js"
 
-test(“adds 5 + 7 to equal 12", () => {
+test("adds 5 + 7 to equal 12", () => {
   expect(sum(5, 7)).toBe(12);
 });
