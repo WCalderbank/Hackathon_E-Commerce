0f738a204c66e304cb9a61161e2085cf9a7db544
Erdogan90 <94233632+Erdogan90@users.noreply.github.com>
2022-06-10 13:59:12 +0100

    presenting data imporvements

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,44 +1,17 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import Card from "react-bootstrap/Card";
-import BasicExample from "./components/Card";
+import ContentCard from "./components/Card";
 
 function App() {
-  const [data, setData] = useState("");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("https://fakestoreapi.com/products");
-      const data = await res.json();
-      setData(data);
-    };
-    fetchData();
-  }, []);
-
-  console.log(data);
 
   return (
     <div className="App">
       <h1 style={{ textAlign: "center", fontWeight: 200 }}>
         Welcome to NORTH & SOUTH FACE
       </h1>
-      <div>
-      {/* {data.map((a, id)=>(
-        <h3 key={id}>{a.title}</h3>
-      ))} */}
-      
-        {Array.from(data).map((a, id) => (
-          <Card key={id} style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={a.image} />
-            <Card.Body>
-              <Card.Title>{a.title}</Card.Title>
-              <Card.Text>{a.description}</Card.Text>
-              <Card.Text>{a.category}</Card.Text>
-            </Card.Body>
-          </Card>
-        ))}
-        ;
-      </div>
+    <ContentCard/>
     </div>
   );
 }
--- e-commerce/src/components/Card.js
+++ e-commerce/src/components/Card.js
@@ -1,20 +1,41 @@
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { useState, useEffect } from "react";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import css from '../styles/card.module.css'
 
-function BasicExample() {
+function ContentCard() {
+    const [data, setData] = useState("");
+
+    useEffect(() => {
+        const fetchData = async () => {
+          const res = await fetch("https://fakestoreapi.com/products");
+          const data = await res.json();
+          setData(data);
+        };
+        fetchData();
+      }, []);
+    
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    <div className={css.cont}>
+    
+    {Array.from(data).map((a, id) => (
+        <Row key={id} className={css.row}>
+        <Col className={css.col} >
+          <Card  style={{ width: "12rem" }}>
+            <Card.Img className={css.image} variant="top" src={a.image} />
+            <Card.Body>
+              <Card.Title>{a.title}</Card.Title>
+              <Card.Text>{a.description}</Card.Text>
+              <Card.Text>{a.category}</Card.Text>
+            </Card.Body>
+          </Card>
+          </Col>
+          </Row>
+          ))}
+    
+    </div>
   );
 }
 
-export default BasicExample;
+export default ContentCard;
