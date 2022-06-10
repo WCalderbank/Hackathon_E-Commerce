ccc31b1ae274e2b5e4435789fa37d5e8360e192f
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2022-06-10 11:29:58 +0100

    initial commit

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,45 +1,41 @@
 import "./App.css";
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
+import Card from "react-bootstrap/Card";
 
 function App() {
+  const [data, setData] = useState("");
 
-  const[data, setData] = useState('')
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("https://fakestoreapi.com/products");
+      const data = await res.json();
+      setData(data);
+    };
+    fetchData();
+  }, []);
 
-  useEffect(()=>{
+  console.log(data);
 
-    const fetchData = async () =>{
-      const res = await fetch ("https://fakestoreapi.com/products")
-      const data = await res.json()
-      setData(data)
-    }
-    fetchData()
-  }, [])
-
- 
-
-
-  console.log(data)
-
-
-  return <div className="App">Hi team
-      <h1 style={{ textAlign: 'center', fontWeight: 200 }} >Welcome to NORTH & SOUTH FACE</h1>
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: "center", fontWeight: 200 }}>
+        Welcome to NORTH & SOUTH FACE
+      </h1>
       <div>
-        {data.map((a, id)=>(
-          <div key={id}>
-          <h4>
-            {a.title}
-            </h4>
-          <img src={a.image} alt={a.category}
-             style={{
-                                    height: '50%',
-                                    width: '50%',
-                                    borderRadius: '10%',
-                                }}
-          />
-          <p>{a.description}</p>
-            <h6>{a.category}</h6>
-          </div>
-        )
-        )}
+        {data.map((a, id) => (
+          <Card key={id} style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={a.image} />
+            <Card.Body>
+              <Card.Title>{a.title}</Card.Title>
+              <Card.Text>{a.description}</Card.Text>
+              <Card.Text>{a.category}</Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
+        ;
       </div>
-      </div>;
+    </div>
+  );
+}
+
+export default App();
